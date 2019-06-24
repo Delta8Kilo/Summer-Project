0ded2b8f5fbe73690d09d5f0ded32f4fd49f57cd
Danil Khrustov <khrustovd@gmail.com>
2019-06-24 10:47:02 -0500

    refactored some code, streamlined the destroyed building

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var clientw = Math.max(document.documentElement.clientWidth, window.innerWidth |
 var clienth = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 var clientm = Math.min(clientw, clienth)
 
+//Aliases
+let Application = PIXI.Application,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
 
 //Create a Pixi Application
-let app = new PIXI.Application({
+let app = new Application({
   width: (clientm * 1.00),
   height: (clientm * 1.00),
   antialias: true,
@@ -12,3 +17,21 @@ let app = new PIXI.Application({
   autoDensity: true,
   backgroundColor: 0x1B4F72
 });
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.getElementById('playfield').appendChild(app.view)
+
+//load an image and run the `setup` function when it's done
+loader
+  .add("images/cat.png")
+  .load(setup);
+
+//This `setup` function will run when the image has loaded
+function setup() {
+
+  //Create the cat sprite
+  let cat = new Sprite(resources["images/cat.png"].texture);
+  
+  //Add the cat to the stage
+  app.stage.addChild(cat);
+}
